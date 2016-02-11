f791291f03e122d9929b65fde6eb570ce08062d6
sungsik.kim <skystar819@gmail.com>
2016-02-11 20:01:59 +0900

    modify core.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "2.0.1"]
-                 [sudharsh/clj-oauth2 "0.5.3"]
-                 [clj-dropbox-oauth2 "0.1.1"]]
+                 [sudharsh/clj-oauth2 "0.5.3"]]
   :main cljdropbox.core)
                 
--- src/cljdropbox/core.clj
+++ src/cljdropbox/core.clj
@@ -1,25 +1,11 @@
 (ns cljdropbox.core
-  (:require [clj-dropbox-oauth2.dropbox :as auth2dropbox]
-            [clj-oauth2.client :as oauth2]
+  (:require [clj-oauth2.client :as oauth2]
             [cheshire.core :as json]
             [clj-http.client :as httpclient]))
 
 ; access-token 얻어오기
 (defn get-access-token []
   (:access_token (load-file "./info.env")))
-
-; 현재 폴더의 파일 갯수
-(defn file-counts [access-token path]
-  (count (filter (fn [x] (false? (:is_dir x)))
-                 (:contents (auth2dropbox/metadata access-token path)))))
-
-; 현재 폴더의 파일 전체 크기
-(defn folder-file-size [access-token path]
-  (reduce (fn [acc x] (+ acc (:bytes x))) 0
-          (:contents (auth2dropbox/metadata access-token ""))))
-
-;(auth2dropbox/metadata (get-access-token) "")
-
 
 (defn parse-oauth2 [method access-token url params]
   (json/parse-string
