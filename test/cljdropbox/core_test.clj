4b2560f64c85085a49b98034139782105f5b6cfd
sungsik.kim <skystar819@gmail.com>
2016-02-18 18:55:26 +0900

    delete duplicated test code

--- test/cljdropbox/core_test.clj
+++ test/cljdropbox/core_test.clj
@@ -7,24 +7,18 @@
 (def tag-file {:.tag "file",})
 (def tag-folder {:.tag "folder"})
 
-(defn mock-files-map [file-counts folder-counts]
+(defn mock-folder-files-map [file-counts folder-counts]
   {:entries (into (into [] (repeat file-counts tag-file)) (repeat folder-counts tag-folder)),
    :cursor "1234567", :has_more false })
 
 ;(dropbox/dropbox-list-folder test-access-token {:path "" :recursive true})
-(defn mock-list-folder-1files [params]
-  (mock-files-map 1 0))
 
-(defn mock-list-folder-2files [params]
-  (mock-files-map 2 0))
-
-(defn mock-list-1folder-3files [params]
-  (mock-files-map 3 1))
-
-((dropbox/get-file-counts mock-list-folder-1files) {:path ""})
+(defn mock-list-folder [file-counts folder-counts]
+  (fn [params]
+    (mock-folder-files-map file-counts folder-counts)))
 
 (deftest file-counts
-  (is (= 1 ((dropbox/get-file-counts mock-list-folder-1files) {:path ""})))
-  (is (= 2 ((dropbox/get-file-counts mock-list-folder-2files) {:path ""})))
-  (is (= 3 ((dropbox/get-file-counts mock-list-1folder-3files) {:path ""}))))
+  (is (= 1 ((dropbox/get-file-counts (mock-list-folder 1 0)) {:path ""})))
+  (is (= 2 ((dropbox/get-file-counts (mock-list-folder 2 0)) {:path ""})))
+  (is (= 3 ((dropbox/get-file-counts (mock-list-folder 3 1)) {:path ""}))))
 
